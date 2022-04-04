0aa7696a84bd608643a905b441aed0c31a09a200
luissepulveda <luissepulveda1996@gmail.com>
2022-04-04 10:27:14 -0400

    Navbar - footer finalizado

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("a").click(function(){
+        var gato = this.hash
+        $("html, body").animate({
+            scrollTop : $(gato).offset().top
+        }, 800)
+    })
+})
