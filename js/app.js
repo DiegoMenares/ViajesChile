935e103c8ecc719d0a34de908cc08260a126a6b7
Fredy <fredyonage@gmail.com>
2022-04-06 21:58:54 -0400

    cambio de fotos y csss y js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
-$(document).ready(function () {
-    $("a").click(function(){
-        var gato = this.hash
-        $("html, body").animate({
-            scrollTop : $(gato).offset().top
-        }, 800)
-    })
+$(document).ready(function() {
+    $("a").click(function() {
+            var gato = this.hash
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 800)
+        })
+        // Card-body desaparece al hacer click sobre las imagenes
+    $('.desaparecer').click(function() {
+        $('.aparece-desaparece').toggle();
+    });
 })
 
 // ALERTA EN BOTON DE FORMULARIO
 let botonEnviar = document.querySelector(".botonEnviar")
-botonEnviar.addEventListener("click", function () {
+botonEnviar.addEventListener("click", function() {
     alert("Mensaje Enviado con exito...")
 })
