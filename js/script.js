2706116240fbeda09190c6e1d57f5cd3bd816af3
Diego Menares <dkmenares@gmail.com>
2022-04-07 22:22:05 -0400

    Proyecto terminado, correcciones en html, css y js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
+// DOCUMENT READY PREVIO PARA QUE ESPERE A CARGAR EL HTML ANTES
+// CODIGO CON SINTAXIS DE JQUERY
 $(document).ready(function () {
+
+  // FUNCION PARA APLICAR TOOLTIP
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  });
+
+
+  // CLICK SOBRE LA FOTO DE LAS CARD HACE DESAPARECER SU CONTENIDO (TITULO Y PARRAFO)
+  $(".desaparecer").click(function () {
+    $(".aparece-desaparece").toggle();
+  });
+
+
+  // CAMBIAR COLOR DEL TEXTO DE LA SECCION QUIENES SOMOS
+  $('.change').on('dblclick', function () {
+    $(this).css({
+      "color": "red"
+    })
+  });
+
+  // SMOOHT SCROLL
   $("a").click(function () {
     var gato = this.hash;
     $("html, body").animate(
@@ -8,11 +32,9 @@ $(document).ready(function () {
       800
     );
   });
-  // Card-body desaparece al hacer click sobre las imagenes
-  $(".desaparecer").click(function () {
-    $(".aparece-desaparece").toggle();
-  });
-});
+
+})
+
 
 // ALERTA EN BOTON DE FORMULARIO CON JS
 let botonEnviar = document.querySelector(".botonEnviar");
